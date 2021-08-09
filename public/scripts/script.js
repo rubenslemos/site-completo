2976d80559cabca4c4e357b23ca5e6d66f004a37
rubenslemos <rubenslemos@gmail.com>
2021-08-09 08:32:10 -0300

    testes

--- public/scripts/script.js
+++ public/scripts/script.js
@@ -1,3 +1,5 @@
+const insert = require('./Insert')
+
 document.querySelector('.hamburger').addEventListener('click', () =>
     document.querySelector('.container').classList.toggle('show-menu')
 );
